9615107956e25b03e9c8a22cdcf01d3f016a9f2d
Jermayn <67791136+Jermayn1@users.noreply.github.com>
2025-07-02 18:55:59 +0200

    Simplen Bot erstellt
    Bot erstellt, welcher Startet (ohne Funktion, Bot geht nur online)

---
+++ src/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds
+    ]
+});
+
+client.login(process.env.BOT_TOKEN);
