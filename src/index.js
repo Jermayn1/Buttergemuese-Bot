3aa3a3af3759b7a62354adc6827fa9359f4dcd5c
Jermayn <67791136+Jermayn1@users.noreply.github.com>
2025-07-02 20:21:47 +0200

    nachtrag, um fehler zuverhindern
    bei einen leeren Channel hab es einen error, behebt das wir kurzzeitig das Riat Troll Feature haben

--- src/index.js
+++ src/index.js
@@ -18,27 +18,27 @@ const blockedUsers = [
 // Bitte nicht ernst nehmen
 // Discord Voice State Update Event
 client.on('voiceStateUpdate', (oldState, newState) => {
-    // Riat Discord ID
-    const monitoredUserId = "1251534422885404718";
+    try {
+        // Riat Discord ID
+        const monitoredUserId = "1251534422885404718";
 
-    // Prüft, ob es um Riat geht
-    if (newState.member.id === monitoredUserId) {
-        const channel = newState.channel;
+        // Prüft, ob es um Riat geht
+        if (newState.member.id === monitoredUserId) {
+            const channel = newState.channel;
 
-        // Prüft, ob ein geschützer User den Channel joint
-        const foundBlockedUser = channel.members.some(member =>
-            blockedUsers.includes(member.id) && member.id !== monitoredUserId
-        );
+            // Prüft, ob ein geschützer User den Channel joint
+            const foundBlockedUser = channel.members.some(member =>
+                blockedUsers.includes(member.id) && member.id !== monitoredUserId
+            );
 
-        // Riat ist mit einer schützen Person im Voice
-        if (foundBlockedUser) {
-            // Kickt Riat aus dem Voice Channel
-            try {
-                newState.disconnect("ANTI RIAT VOICE SYSTEM");
-            } catch (err) {
-                // Keine Fehler ausgeben, DANKE!
+            // Riat ist mit einer schützen Person im Voice
+            if (foundBlockedUser) {
+                // Kickt Riat aus dem Voice Channel
+                    newState.disconnect("ANTI RIAT VOICE SYSTEM");
             }
         }
+    } catch (err) {
+        // Keine Fehler ausgeben, DANKE!
     }
 });
 
