a3d2bb22fa5ed70875a10dfb5ecbf1fb6ee64e11
Jermayn <67791136+Jermayn1@users.noreply.github.com>
2025-07-02 20:20:33 +0200

    Kurzes temporäres Troll Feature
    Dient zum kruzzeitigen Troll und wir später entfernt. Zeigt nur was wir für möglichkeiten haben mit einen Discord Bot.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,43 @@ const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates
     ]
 });
+
+// Geschütze User vor Riat
+const blockedUsers = [
+  "380066508425920522"
+];
+
+// Hierbei handelt es sich um ein kurzes Troll Feature
+// Bitte nicht ernst nehmen
+// Discord Voice State Update Event
+client.on('voiceStateUpdate', (oldState, newState) => {
+    // Riat Discord ID
+    const monitoredUserId = "1251534422885404718";
+
+    // Prüft, ob es um Riat geht
+    if (newState.member.id === monitoredUserId) {
+        const channel = newState.channel;
+
+        // Prüft, ob ein geschützer User den Channel joint
+        const foundBlockedUser = channel.members.some(member =>
+            blockedUsers.includes(member.id) && member.id !== monitoredUserId
+        );
+
+        // Riat ist mit einer schützen Person im Voice
+        if (foundBlockedUser) {
+            // Kickt Riat aus dem Voice Channel
+            try {
+                newState.disconnect("ANTI RIAT VOICE SYSTEM");
+            } catch (err) {
+                // Keine Fehler ausgeben, DANKE!
+            }
+        }
+    }
+});
+
 
 client.login(process.env.BOT_TOKEN);
