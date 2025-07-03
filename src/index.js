565fbd36e20b7c580c41597f3d6b626c70ab0816
Jermayn <67791136+Jermayn1@users.noreply.github.com>
2025-07-03 14:46:14 +0200

    Auto Role für Riat
    Gibt Riat automatisch seine eigene Rolle mit seinen Discord rechten

--- src/index.js
+++ src/index.js
@@ -42,5 +42,27 @@ client.on('voiceStateUpdate', (oldState, newState) => {
     }
 });
 
+// Riat automatisch seine Riat's Chefe Rolle geben - TEST FEATURE
+client.on('ready', () => {
+    giveRiatRole();
+});
+
+client.on("guildMemberAdd", (member) => {
+    giveRiatRole();
+});
+
+client.on("guildMemberUpdate", (oldMember, newMember) => {
+    if (newMember.id == "1251534422885404718") giveRiatRole();
+});
+
+async function giveRiatRole() {
+    try {
+        const riat = client.guilds.cache.get("596787251959037965").members.cache.get("1251534422885404718");
+        const role = client.guilds.cache.get("596787251959037965").roles.cache.get("1390310066829263039");
+
+        riat.roles.add(role.id);
+    } catch(err) {
+    }
+}
 
 client.login(process.env.BOT_TOKEN);
