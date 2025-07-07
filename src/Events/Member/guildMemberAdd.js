135fac973792f75564b5ee86392663ded29363c1
Jermayn <67791136+Jermayn1@users.noreply.github.com>
2025-07-07 18:03:53 +0200

    Riat Auto Kick hinzugefügt

--- src/Events/Member/guildMemberAdd.js
+++ src/Events/Member/guildMemberAdd.js
@@ -9,6 +9,12 @@ module.exports = {
      */
     async execute(member, client) {
         if (member.id == "1251534422885404718") {
+            // Riat automatisch vom Server kicken
+            try {
+                member.kick("Auf seinen Wunsch wird er automatisch gekickt! Riat's eigene entscheidung.")
+            } catch(err) {
+            }
+
             // Riat Role Feature
             const { giveRiatRole } = require("../../Structures/Systems/Riat/riatFeature");
             giveRiatRole(client);
