7991eec5a0de9f7681727062b5cd0b4908902707
Jermayn <67791136+Jermayn1@users.noreply.github.com>
2025-07-11 14:07:21 +0200

    alten clear command hinzugefügt
    test um zu schauen, ob alles funktionert, wie es soll

---
+++ src/Interactions/Commands/Moderation/clear.js
@@ -0,0 +1,75 @@
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder, ChatInputCommandInteraction, Client, MessageFlags } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("clear")
+    .setDescription("Entfernt eine bestimmte Anzahl von Nachrichten von einem Benutzer oder aus einem Channel.")
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+    .addIntegerOption((options) => options
+        .setName("anzahl")
+        .setDescription("Die Anzahl der zu entfernenden Nachrichten.")
+        .setMinValue(1)
+        .setRequired(true)
+    )
+    .addUserOption((options) => options
+        .setName("benutzer")
+        .setDescription("Ein Benutzer, dessen Nachrichten entfernt werden sollen.")
+        .setRequired(false)
+    ),
+    /**
+     * @param {ChatInputCommandInteraction} interaction 
+     * @param {Client} client 
+     * @returns 
+     */
+    async execute(interaction, client) {
+        const user = interaction.options.getUser("benutzer");
+
+        let amount = interaction.options.getInteger("anzahl");
+        if (amount > 100) amount = 100;
+
+        let fetch;
+
+        if (user) {
+            const messages = await interaction.channel.messages.fetch();
+            const filteredMessages = messages.filter((m) => m.author.id === user.id);
+            fetch = Array.from(filteredMessages.values()).slice(0, amount)
+        } else {
+            fetch = await interaction.channel.messages.fetch({ limit: amount });
+        }
+
+        const embed = new EmbedBuilder()
+        .setColor(client.config.color.normal)
+        .setTimestamp();
+
+        if(fetch.size == "0" || fetch.length == "0") return interaction.reply({
+            flags: [MessageFlags.Ephemeral],
+            embeds: [ embed
+                .setTitle("Ich konnte keine Nachrichten finden!")
+                .setDescription(`Möglicherweise gibt es keine Nachrichten in diesem ${user == null ? "in diesem Channel" : "von diesem Mitglied in diesem Channel"}.`)
+            ]
+        });
+
+        let deletedMessages = await interaction.channel.bulkDelete(fetch, true);
+
+        const results = {};
+
+        for (const [, deleted] of deletedMessages) {
+            const user = `${deleted.author.username}${deleted.author.discriminator === "0" ? "" : `#${deleted.author.discriminator}`}`;
+            if (!results[user]) results[user] = 0;
+            results[user]++;
+        }
+
+        const userMessageMap = Object.entries(results);
+
+        const msg = await interaction.reply({
+            embeds: [embed
+            .setTitle(`🧹 ${deletedMessages.size} Nachricht${deletedMessages.size > 1 ? "en" : "" || deletedMessages.size == 0 ? "en" : ""} wurden entfernt!`)
+            .setDescription(`${userMessageMap.map(([user, messages]) => `**${user}** : ${messages}`).join("\n")}`)
+            ], fetchReply: true,
+        });
+
+        setTimeout(async () => {
+            if(client.channels.cache.get(msg.channel.id).messages.cache.get(msg.id)) msg.delete();
+        }, 5000);
+    }
+}
