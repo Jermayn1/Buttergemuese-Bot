d2c698e721d2fc810dc46330770c5031dc65da2e
Jermayn <67791136+Jermayn1@users.noreply.github.com>
2025-07-11 16:47:02 +0200

    Anti Melina Voice System

---
+++ src/Events/Voice/antiMelinaSystem.js
@@ -0,0 +1,41 @@
+const { VoiceState, Client } = require("discord.js");
+
+// Geschütze User vor Riat
+const blockedUsers = [
+  "380066508425920522" // Jermayn
+];
+
+module.exports = {
+    name: "voiceStateUpdate",
+    once: false,
+    /**
+     * @param {VoiceState} oldState 
+     * @param {VoiceState} newState 
+     * @param {Client} client 
+     */
+    async execute(oldState, newState, client) {
+
+        try {
+            // Anti Melina System
+            const channel = newState.channel;
+            const monitoredUserId = "1166455819223703703";
+
+            // Prüft, ob es um Riat geht
+            if (newState.member.id === monitoredUserId) {
+
+                // Prüft, ob ein geschützer User den Channel joint
+                const foundBlockedUser = channel.members.some(member =>
+                    blockedUsers.includes(member.id) && member.id !== monitoredUserId
+                );
+
+                // Melina ist mit einer schützen Person im Voice
+                if (foundBlockedUser) {
+                    // Kickt Melina aus dem Voice Channel
+                        newState.disconnect("ANTI MELINA VOICE SYSTEM");
+                }
+            }
+        } catch (err) {
+            // Keine Fehler ausgeben, DANKE!
+        }
+    }
+}
